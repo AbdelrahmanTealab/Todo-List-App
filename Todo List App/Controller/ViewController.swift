71f51f9913b6dbb0c9572fe194b3a01936245b47
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2020-11-29 14:05:27 -0500

    added firebase

--- Todo List App/Controller/ViewController.swift
+++ Todo List App/Controller/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         List(title: "Games", dueDate: Date(), items: ["Red Dead","Cyberpunk","Tekken"]),
         List(title: "Productivity", dueDate: Date(), items: ["Finish app UI","Study iOS","Study UIUX"]),
         List(title: "Music", dueDate: Date(), items: ["Her Mannelig","In The Air Tonight","Unshaken"]),
-        List(title: "Random list", dueDate: Date(), items: ["shave beard","trim hair","wash clothes"])
+        List(title: "Some other list", dueDate: Date(), items: ["shave beard","trim hair","wash clothes"])
     ]
 
     override func viewDidLoad() {
